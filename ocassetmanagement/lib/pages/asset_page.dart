ef2e3087c403ff2b85785385e4944036bb35b211
Skyler8212 <129219374+Skyler8212@users.noreply.github.com>
2024-02-07 13:59:49 -0600

    Added extra textinputs
    Added more formatting and form fields

--- ocassetmanagement/lib/pages/asset_page.dart
+++ ocassetmanagement/lib/pages/asset_page.dart
@@ -11,44 +11,109 @@ class AssetPage extends StatelessWidget {
         child: Center(
       child: Form(
           key: _formKey,
-          child: Column(
-              //children: List<Widget>.generate(3, (int index) {
-              children: <Widget>[
+            child: Column(
+              children: [
+                Column(
+                    //children: List<Widget>.generate(3, (int index) {
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SizedBox(
+                              width: 200,
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                  labelText: 'Name',
+                                ),
+                                onSaved: (String? value) {
+                                  //debugPrint('value for field $index saved as "$value"');
+                                },
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Enter something';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                            SizedBox(width: 20),
+                            SizedBox(
+                              width: 200,
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                  labelText: 'Name2',
+                                ),
+                                onSaved: (String? value) {
+                                  //debugPrint('value for field $index saved as "$value"');
+                                },
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Enter something2';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SizedBox(
+                              width: 200,
+                              child: TextFormField(
+                                decoration: const InputDecoration(
+                                  labelText: 'Name3',
+                                ),
+                                onSaved: (String? value) {
+                                  //debugPrint('value for field $index saved as "$value"');
+                                },
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Enter something3';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                              content: Text('Great!'),
+                            ));
+                          }
+                        },
+                        child: const Text('Validate'),
+                      )
+                    ]
+                    ),
+             Column(children: [
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8.0),
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       SizedBox(
-                        width: 300,
+                        width: 200,
                         child: TextFormField(
                           decoration: const InputDecoration(
-                            labelText: 'Name',
+                            labelText: 'Name3',
                           ),
                           onSaved: (String? value) {
                             //debugPrint('value for field $index saved as "$value"');
                           },
                           validator: (value) {
                             if (value == null || value.isEmpty) {
-                              return 'Enter something';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                      SizedBox(width: 20),
-                      SizedBox(
-                        width: 300,
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Name2',
-                          ),
-                          onSaved: (String? value) {
-                            //debugPrint('value for field $index saved as "$value"');
-                          },
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Enter something2';
+                              return 'Enter something3';
                             }
                             return null;
                           },
@@ -57,31 +122,10 @@ class AssetPage extends StatelessWidget {
                     ],
                   ),
                 ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Name3',
-                  ),
-                  onSaved: (String? value) {
-                    //debugPrint('value for field $index saved as "$value"');
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Enter something3';
-                    }
-                    return null;
-                  },
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text('Great!'),
-                      ));
-                    }
-                  },
-                  child: const Text('Validate'),
-                )
-              ])),
+                ]) 
+                ],
+            )
+              ),
     ));
   }
 }
