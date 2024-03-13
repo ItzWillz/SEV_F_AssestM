0df5ecff8a3fca1e32f777a125eebfc7387c6842
jyaheza <mauriceirakoze77@gmail.com>
2024-03-13 18:41:52 -0500

    allUserPage and checkin fixed

--- lib/pages/check_in_and_out_page.dart
+++ lib/pages/check_in_and_out_page.dart
@@ -48,60 +48,29 @@ class _CheckInandOutPageState extends State<CheckInandOutPage> {
   @override
   Widget build(BuildContext context) {
     return Material(
-
-          child: Padding(
-          padding: const EdgeInsets.all(10.0),
-            
-            child: ListView(
-              children: [
-                const Text("    Checked Out Assets ", style: TextStyle( fontSize: 20.0,), textAlign: TextAlign.center,),
-                Row(
-                children: [
-                             Padding(
-                               padding: const EdgeInsets.only(bottom: 22.0, left: 20),
-                               child: SizedBox(
-                                width: 200,
-                                child: TextField(
-                                  onChanged: (value) => setState(() {
-                                    _filter(value);
-                                  }
-                                  ),
-                                  decoration: const InputDecoration(
-                                    labelText: 'Search', suffixIcon: Icon(Icons.search),
-                                  ),
-                                ),
-                                                               ),
-                             ),
-                const Spacer(),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 15.0),
-                    child: SizedBox(
-                              width: 50,
-                              child: IconButton(
-                                  onPressed: (){
-                            final notifier = Provider.of<CreateCheckOutNotifier>(context, listen: false);
-                                  notifier.newCheckOutScreen(asset: null);
-                                     //notifier.completeProfileSelectionScren(assetName: selectedIcon?.label);
-                    
-                                    // Navigator.of(
-                                    //   context
-                                    // ).push(MaterialPageRoute(builder: (context) => const NewCheckOutPage()));
-                                  }, 
-                                icon: const Icon(Icons.add, color: Colors.white,), 
-                                //label: const Text("", style: TextStyle(color: Colors.white)),
-                                style: IconButton.styleFrom(
-                                  backgroundColor: const Color.fromARGB(255, 76, 200, 63),
-                                  //textStyle: const TextStyle(color: Colors.white),
-                                )),
-                                  ),
-                  ),        
-                ] ),
-                Row(
-                  children: [
-                    Expanded(
-                     flex: 4,
-                      child: AssetDataTable(data: _filtered, onViewMore: (Object ) {  }, onEdit: (Object ) {  },), 
-
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          children: [
+            const Text(
+              "    Checked Out Assets ",
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Row(children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 22.0, left: 20),
+                child: SizedBox(
+                  width: 200,
+                  child: TextField(
+                    onChanged: (value) => setState(() {
+                      _filter(value);
+                    }),
+                    decoration: const InputDecoration(
+                      labelText: 'Search',
+                      suffixIcon: Icon(Icons.search),
                     ),
                   ),
                 ),
