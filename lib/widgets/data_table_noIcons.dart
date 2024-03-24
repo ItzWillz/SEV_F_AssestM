15f812e9178fbfd074bd599ba9c2e2426a09024a
jyaheza <mauriceirakoze77@gmail.com>
2024-03-24 17:10:16 -0500

    sorting works

--- lib/widgets/data_table_noIcons.dart
+++ lib/widgets/data_table_noIcons.dart
@@ -7,28 +7,28 @@ import '../models/tableable.dart';
 class MyDataSource extends DataTableSource {
   MyDataSource({required this.data});
 
-  final List<Object> data;
+  final List<Tableable> data;
+  int _sortColumnIndex = 0;
+  bool _sortAscending = true;
 
-  @override
-  int get rowCount => data.length;
-
-  @override
-  bool get isRowCountApproximate => false;
-
-  @override
-  int get selectedRowCount => 0;
+  void sort(
+      Comparable<dynamic> Function(Tableable) sortCriterion, bool ascending) {
+    data.sort((Tableable a, Tableable b) {
+      final Comparable<dynamic> aValue = sortCriterion(a);
+      final Comparable<dynamic> bValue = sortCriterion(b);
+      return ascending ? aValue.compareTo(bValue) : bValue.compareTo(aValue);
+    });
+    _sortAscending = ascending;
+    notifyListeners();
+  }
 
   @override
   DataRow? getRow(int index) {
-    final row = data[index];
-
-    if (row is Tableable) {
-      return DataRow(cells: row.asRow().map(_toDataCell).toList());
-    } else {
-      if (kDebugMode) print('$row doens\'t implement tableable.');
-      return DataRow(
-          cells: List.filled(data.length, const DataCell(Placeholder())));
-    }
+    final Tableable row = data[index];
+    return DataRow.byIndex(
+      index: index,
+      cells: row.asRow().map(_toDataCell).toList(),
+    );
   }
 
   DataCell _toDataCell(Object? label) {
@@ -36,67 +36,73 @@ class MyDataSource extends DataTableSource {
       label.addListener(() => notifyListeners());
       return label.toDataCell();
     }
-
     return DataCell(Text(label?.toString() ?? ''));
   }
+
+  @override
+  int get rowCount => data.length;
+  @override
+  bool get isRowCountApproximate => false;
+  @override
+  int get selectedRowCount => 0;
 }
 
-class DataTableNoIcons extends StatelessWidget {
-  DataTableNoIcons({super.key, required this.data})
-      : dataSource = MyDataSource(data: data);
+class DataTableNoIcons extends StatefulWidget {
+  const DataTableNoIcons({Key? key, required this.data}) : super(key: key);
 
-  final List<Object> data;
-  final DataTableSource dataSource;
+  final List<Tableable> data;
+
+  @override
+  State<DataTableNoIcons> createState() => _DataTableNoIconsState();
+}
+
+class _DataTableNoIconsState extends State<DataTableNoIcons> {
+  late final MyDataSource _dataSource;
+
+  @override
+  void initState() {
+    super.initState();
+    _dataSource = MyDataSource(data: widget.data);
+  }
+
+  void _sort(int columnIndex, bool ascending) {
+    _dataSource.sort((Tableable d) {
+      final value = d.asRow()[columnIndex];
+      return value is Comparable ? value : '';
+    }, ascending);
+
+    setState(() {
+      _dataSource._sortColumnIndex = columnIndex;
+      _dataSource._sortAscending = ascending;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (data.isEmpty) {
-      return const Text('Table is empty');
-    }
-    final asset = data.first;
+    if (widget.data.isEmpty) return const Text('Table is empty');
 
-    if (asset is Tableable) {
-      return PaginatedDataTable(
-        columns: asset.header().map(columnHeader).toList(),
-        source: dataSource,
-      );
-    }
+    final columns = widget.data.first
+        .header()
+        .asMap()
+        .map((index, label) => MapEntry(
+            index,
+            DataColumn(
+              label: Text(label),
+              onSort: (columnIndex, ascending) => _sort(columnIndex, ascending),
+            )))
+        .values
+        .toList();
 
-    return Text(
-        '${asset.runtimeType} does not implement Tableable. Please fix this.');
-  }
-
-  DataColumn columnHeader(String label) {
-    return DataColumn(label: Text(label.toString()));
+    return PaginatedDataTable(
+      columns: columns,
+      source: _dataSource,
+      sortColumnIndex: _dataSource._sortColumnIndex,
+      sortAscending: _dataSource._sortAscending,
+    );
   }
 }
 
-class VendorDataTable extends StatelessWidget {
-  VendorDataTable({super.key, required this.data})
-      : dataSource = MyDataSource(data: data);
-
-  final List<Object> data;
-  final DataTableSource dataSource;
-
-  @override
-  Widget build(BuildContext context) {
-    if (data.isEmpty) {
-      return const Text('Table is empty');
-    }
-    final vendor = data.first;
-
-    if (vendor is Tableable) {
-      return PaginatedDataTable(
-        columns: vendor.header().map(columnHeader).toList(),
-        source: dataSource,
-      );
-    }
-
-    return Text(
-        '${vendor.runtimeType} does not implement Tableable. Please fix this.');
-  }
-
-  DataColumn columnHeader(String label) {
-    return DataColumn(label: Text(label.toString()));
-  }
+class VendorDataTable extends DataTableNoIcons {
+  const VendorDataTable({Key? key, required List<Tableable> data})
+      : super(key: key, data: data);
 }
